76adff8dd642cbe58c5b64d3c4299cb9a41e139f
Michael Herrmann <michael@herrmann.io>
2016-03-01 22:37:46 +0100

    First version that should run a Django web server (behind nginx) and should print "Hello World!" at https://www.excludemyip.com.

---
+++ emi/settings_prod.py
@@ -0,0 +1,46 @@
+from .settings import *
+
+from os import makedirs
+from os.path import exists
+
+import re
+
+from emi.secret_key import SECRET_KEY
+
+LOG_DIR = join(BASE_DIR, 'logs')
+if not exists(LOG_DIR):
+	makedirs(LOG_DIR)
+
+LOGGING['handlers']['default'] = {
+	'level': 'INFO',
+	'class': 'logging.FileHandler',
+	'filename': join(LOG_DIR, 'django.log')
+}
+
+DEBUG = False
+
+MIDDLEWARE_CLASSES.append(
+	'django.middleware.common.BrokenLinkEmailsMiddleware'
+)
+
+IGNORABLE_404_URLS = [
+	re.compile(r'^/favicon\.ico$')
+]
+
+# Use Postfix:
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/emi/src/static-collected/'
+
+ALLOWED_HOSTS = ('www.excludemyip.com',)
+
+# SSL Settings:
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
